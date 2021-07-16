648aad474e67b4b5c7a89686364c759a382875c7
sinazay123 <sinazay123@gmail.com>
2021-07-16 11:21:57 -0500

    agregar cors y otros middlewares

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-console */
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
 const api = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +12,12 @@ const { Server } = require('socket.io');
 
 const io = new Server(server);
 
-api.use(require('../routers'));
+api.use(express.json());
+api.use(express.urlencoded({ extended: true }));
+api.use(morgan('dev'));
+api.use(cors({}));
+
+api.use('/api/v1', require('../routers'));
 
 api.get('/', (req, res) => {
   res.send('Welcome.');
