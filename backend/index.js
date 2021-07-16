253f6b6135a07100919f38708a0e5232de4dd2ad
sinazay123 <sinazay123@gmail.com>
2021-07-16 09:14:17 -0500

    Agregar coneccion la base de datos

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 /* eslint-disable no-console */
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
 const { server, PORT } = require('./api');
 
-server.listen(PORT, ()=> {
+server.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
-})
+});
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to DB.'))
+  .catch((err) => console.log('Error while connecting to DB', err));
