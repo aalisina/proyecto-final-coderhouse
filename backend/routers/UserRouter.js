f256fe9d69400b20d2737b4e5a74fb23cd43a844
sinazay123 <sinazay123@gmail.com>
2021-07-16 22:52:05 -0500

    agregar validator en route de usuario

--- backend/routers/UserRouter.js
+++ backend/routers/UserRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { UserController } = require('../controllers');
+const { UserValidator } = require('../validators');
 
 const router = express.Router();
 
 router.get('/', UserController.getAll);
 
-router.post('/', UserController.create);
+router.post('/', UserValidator.create, UserController.create);
 
 module.exports = router;
