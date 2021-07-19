de6fd916d30898d23d065ee5500db95d6d1ee5e2
sinazay123 <sinazay123@gmail.com>
2021-07-19 13:22:15 -0500

    Modificar options de coneccion de mongo

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,11 @@ server.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  dbName: 'mongo-coder',
+})
   .then(() => console.log('Connected to DB.'))
   .catch((err) => console.log('Error while connecting to DB', err));
