89d3f92b9be8fa606cd9ff0e1aa2259ed3dde093
sinazay123 <sinazay123@gmail.com>
2021-07-19 15:51:17 -0500

    agregar validacion por si usuario no existe al traer un usuario

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -13,6 +13,7 @@ module.exports = {
     const { id } = req.params;
     try {
       const user = await UserService.getOne(id);
+      if (!user) return res.status(404).json({message: 'User not found.'});
       res.status(200).json(user);
     } catch (error) {
       res.status(400).json(error);
