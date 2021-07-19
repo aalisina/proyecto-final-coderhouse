993a15c6176959161e6f78e51a4b3aff10c7b97d
sinazay123 <sinazay123@gmail.com>
2021-07-19 16:49:05 -0500

    separar rutas de login y signup

--- backend/routers/index.js
+++ backend/routers/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/users', require('./UserRouter'));
-router.use('/signup', require('./AuthRouter'));
+router.use('/signup', require('./SignupRouter'));
+router.use('/login', require('./LoginRouter'));
 
 module.exports = router;
