cd12e4fd2b66f7af427d29c96bb38ee1544028ff
sinazay123 <sinazay123@gmail.com>
2021-07-19 14:44:50 -0500

    agregar validator para actualizar un usuario

--- backend/validators/UserValidator.js
+++ backend/validators/UserValidator.js
@@ -14,4 +14,16 @@ module.exports = {
       id: Joi.string().required(),
     }),
   }),
+  update: celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.string().required(),
+    }),
+    [Segments.BODY]: Joi.object().keys({
+      first_name: Joi.string(),
+      last_name: Joi.string(),
+      email: Joi.string().email(),
+      password: Joi.string(),
+    }),
+
+  }),
 };
