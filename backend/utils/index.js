fa4a2f7ab17a5e814b48b30c8469b3aac16b61a1
sinazay123 <sinazay123@gmail.com>
2021-07-20 10:53:47 -0500

    Agregar try/catch para el metodo de creacion de token y tiempo de expircacion

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -4,17 +4,22 @@ const jwt = require('jsonwebtoken');
 module.exports = {
   comparePasswords: (userPassword, reqPassword) => bcrypt.compareSync(reqPassword, userPassword),
   createToken: (user) => {
-    const token = jwt.sign(
-      {
-        // eslint-disable-next-line no-underscore-dangle
-        _id: user._id,
-        first_name: user.first_name,
-        email: user.email,
-        orders: user.orders,
-        admin: user.admin,
-      },
-      process.env.JWT_SECRET,
-    );
-    return token;
+    try {
+      const token = jwt.sign(
+        {
+          // eslint-disable-next-line no-underscore-dangle
+          _id: user._id,
+          first_name: user.first_name,
+          email: user.email,
+          orders: user.orders,
+          admin: user.admin,
+          exp: Math.floor(Date.now() / 1000) + (60 * 60),
+        },
+        process.env.JWT_SECRET,
+      );
+      return token;
+    } catch (error) {
+      return undefined;
+    }
   },
 };
