e5916d4529fdf3cbbb031763d44f6db364366e29
sinazay123 <sinazay123@gmail.com>
2021-07-20 16:04:10 -0500

    agregar rutas de producto

---
+++ backend/routers/ProductsRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { ProductsController } = require('../controllers');
+const { ProductsValidator } = require('../validators');
+
+const router = express.Router();
+
+router.get('/', ProductsController.getAll);
+router.get('/:category', ProductsValidator.getProductByCategory, ProductsController.getProductByCategory);
+router.post('/', ProductsValidator.create, ProductsController.create);
+router.patch('/:id', ProductsValidator.update, ProductsController.update);
+router.delete('/:id', ProductsValidator.delete, ProductsController.delete);
+
+module.exports = router;
--- backend/routers/index.js
+++ backend/routers/index.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.use('/users', verifyTokenAdmin, require('./UserRouter'));
 router.use('/signup', require('./SignupRouter'));
 router.use('/login', require('./LoginRouter'));
+router.use('/products', require('./ProductsRoute'));
 
 module.exports = router;
