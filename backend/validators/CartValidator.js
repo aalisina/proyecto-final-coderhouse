daa420dde1cd6e8d97e44f282d8075ee31fde850
sinazay123 <sinazay123@gmail.com>
2021-07-26 17:05:12 -0500

    agregar validacion para las rutas del carrito

--- backend/validators/CartValidator.js
+++ backend/validators/CartValidator.js
@@ -1,38 +1,16 @@
 const { celebrate, Joi, Segments } = require('celebrate');
 
 module.exports = {
-  getUserCart: celebrate({
-  }),
-  create: celebrate({
+  addProductToCart: celebrate({
     [Segments.BODY]: Joi.object().keys({
-      user_id: Joi.string().required(),
-      products: Joi.array().items({
-        _id: Joi.string().required(),
-        quantity: Joi.number().required(),
-      }),
-      address: Joi.object().keys({
-        street: Joi.string().required(),
-        height: Joi.string().required(),
-        postal_code: Joi.number().required(),
-        floor: Joi.string(),
-        department: Joi.string(),
-      }),
+      product_id: Joi.string().required(),
+      quantity: Joi.number().required(),
     }),
   }),
-  update: celebrate({
+  deleteProductFromCart: celebrate({
     [Segments.BODY]: Joi.object().keys({
-      user_id: Joi.string(),
-      products: Joi.array().items({
-        _id: Joi.string(),
-        quantity: Joi.number(),
-      }),
-      address: Joi.object().keys({
-        street: Joi.string(),
-        height: Joi.string(),
-        postal_code: Joi.number(),
-        floor: Joi.string(),
-        department: Joi.string(),
-      }),
+      quantity: Joi.number().required(),
+      product_id: Joi.string().required(),
     }),
   }),
 };
