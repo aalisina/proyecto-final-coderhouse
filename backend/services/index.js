c5a2b9343b709f98a0902189582731f713eb3c5d
sinazay123 <sinazay123@gmail.com>
2021-07-26 17:04:40 -0500

    exportar CartServic desde el indice de la carpeta

--- backend/services/index.js
+++ backend/services/index.js
@@ -1,6 +1,7 @@
 const UserService = require('./UserService');
 const ProductsService = require('./ProductsService');
+const CartService = require('./CartService');
 
 module.exports = {
-  UserService, ProductsService,
+  UserService, ProductsService, CartService,
 };
