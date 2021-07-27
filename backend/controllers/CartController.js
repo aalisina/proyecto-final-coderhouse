991d65b2a518c0bc73f35e1c737e63e2582948f9
sinazay123 <sinazay123@gmail.com>
2021-07-27 15:26:45 -0500

    agregar logica de borrar productos del carrito segun la cantidad y agregar productos segun la cantidad; TODO: hacer refactor para pasar la logica del modelo al servicio del carrito

--- backend/controllers/CartController.js
+++ backend/controllers/CartController.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable camelcase */
-const mongoose = require('mongoose');
 const { CartService, ProductsService } = require('../services');
 const { Cart } = require('../models');
 
@@ -40,13 +39,9 @@ module.exports = {
       if (product.stock < body.quantity || product.stock <= 0) return res.status(400).json({ message: 'Out of stock.' });
       const stock = (product.stock - body.quantity);
       await ProductsService.updateQuantity(product._id, stock);
-      // no funciona
-      // cart.products.push(body).save();
-
-      // probar con otra instancia del carrito
       const anotherCart = await Cart.findById(cart._id);
       anotherCart.products.push(body);
-      const updatedCart = await Cart.findByIdAndUpdate(cart._id, anotherCart)
+      const updatedCart = await Cart.findByIdAndUpdate(cart._id, anotherCart);
 
       res.status(200).json(updatedCart);
     } catch (error) {
@@ -54,14 +49,37 @@ module.exports = {
     }
   },
   deleteProductFromCart: async (req, res) => {
+    const { body } = req;
     const idUser = req.decoded._id;
     try {
       const carts = await CartService.getAllCarts();
       if (!carts) res.status(400).json({ message: 'Carts not found.' });
-      const cart = carts.filter((e) => (e.user_id.toString() === idUser));
+      const cart = carts.filter((e) => (e.user_id.toString() === idUser))[0];
       if (!cart) res.status(400).json({ message: 'Cart not found.' });
-      await CartService.delete(cart._id);
-      res.status(204).json({});
+      const products = await ProductsService.getAll();
+      const product = products.filter((e) => e._id.toString() === body.product_id)[0];
+      if (!product) res.status(400).json({ message: 'Product not found.' });
+      const stock = (product.stock + body.quantity);
+      await ProductsService.updateQuantity(product._id, stock);
+
+      const anotherCart = await Cart.findById(cart._id);
+
+      const resCart = anotherCart.products.map((e) => {
+        if ((e.quantity - body.quantity) === 0
+        && e.product_id === body.product_id) {
+          return undefined;
+        }
+        if (e.product_id === body.product_id) {
+          return {
+            product_id: e.product_id,
+            quantity: e.quantity - body.quantity,
+          };
+        }
+        return e;
+      });
+      anotherCart.products = resCart;
+      anotherCart.save();
+      res.status(200).json(resCart);
     } catch (error) {
       res.status(400).json(error);
     }
