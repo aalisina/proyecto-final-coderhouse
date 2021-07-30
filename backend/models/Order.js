47c1d9876cd0b6fdb348fa6b93211a1e54910060
sinazay123 <sinazay123@gmail.com>
2021-07-30 15:58:02 -0500

    modelo de ordenes agregado

---
+++ backend/models/Order.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+const orderSchema = new Schema({
+  user_id: {
+    type: Schema.Types.ObjectId,
+  },
+  items: [{
+    product_id: String,
+    quantity: Number,
+    price: Number,
+  }],
+  state: {
+    type: String,
+    enum: ['GENERATED', 'PAID', 'SENT', 'FINALIZED'],
+    default: 'GENERATED',
+  },
+}, {
+  timestamps: true,
+  versionKey: false,
+});
+
+const Order = model('order', orderSchema, 'order');
+
+module.exports = {
+  Order,
+};
