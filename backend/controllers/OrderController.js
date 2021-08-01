83d2558f74470d262d4d8fd478517b3cdb4353cf
sinazay123 <sinazay123@gmail.com>
2021-08-01 15:41:34 -0500

    agregar la ruta de traer un orden de usuario por id pasado por params

--- backend/controllers/OrderController.js
+++ backend/controllers/OrderController.js
@@ -15,5 +15,20 @@ module.exports = {
       res.status(400).json(error);
     }
   },
+  getOrderById: async (req, res) => {
+    const idUser = req.decoded._id;
+    const idOrder = req.params.id;
+    try {
+      const allOrders = await OrderService.getAllOrders();
+      // eslint-disable-next-line max-len
+      const userOrders = allOrders.filter((order) => order.user_id.toString() === idUser.toString());
+      if (!userOrders) res.status(400).json({ message: 'User has no orders.' });
+      const order = userOrders.filter((ord) => ord._id.toString() === idOrder.toString());
+      if (!order) res.status(400).json({ message: 'Order not found.' });
+      res.status(200).json(order);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  },
 
 };
--- backend/routers/OrderRouter.js
+++ backend/routers/OrderRouter.js
@@ -5,5 +5,6 @@ const { OrderValidator } = require('../validators');
 const router = express.Router();
 
 router.get('/', OrderController.getAllUserOrders);
+router.get('/:id', OrderController.getOrderById);
 
 module.exports = router;
