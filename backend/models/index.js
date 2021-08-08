b4b6555d8657d9c149653b3a8566ecf30e4fe323
sinazay123 <sinazay123@gmail.com>
2021-08-08 13:03:37 -0500

    exportar el modelo de mensajes desde el indice de la carpeta

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,7 +2,8 @@ const { User } = require('./User');
 const { Product } = require('./Product');
 const { Cart } = require('./Cart');
 const { Order } = require('./Order');
+const { Message } = require('./Message');
 
 module.exports = {
-  User, Product, Cart, Order,
+  User, Product, Cart, Order, Message,
 };
